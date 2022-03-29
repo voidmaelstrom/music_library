e88be3b91500e358d6ab581fdc36630e566e8388
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-28 21:00:57 -0600

    Cleanup whitespace

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,13 +5,10 @@ function SearchBar(props) {
 
     return (
         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
             <input type="text" placeholder="Enter a search term here" onChange={
                 (e) => setSearchTerm(e.target.value)
             } />
-
             <input type="submit" />
-
         </form>
     )
 }
