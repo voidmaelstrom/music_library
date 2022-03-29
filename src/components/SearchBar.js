570d851bdd6112a7d67490003dd3d6ddb7b119a5
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-28 20:54:30 -0600

    Some fixes for search and separate route for album vs. artist

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,18 @@
-function SearchBar(props){
-    // We can comment out our searchTerm state variable as we are not using it!
-    // let [searchTerm, setSearchTerm] = useState('')
+import { useState } from 'react'
+
+function SearchBar(props) {
+    let [searchTerm, setSearchTerm] = useState('')
 
     return (
-            <form>
-                <input type="text" placeholder="Search Here"
-                    onChange={
-                        (e) => props.handleSearch(e, e.target.value)
-                    } />
-                <input type="submit" />
-            </form>
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+
+            <input type="text" placeholder="Enter a search term here" onChange={
+                (e) => setSearchTerm(e.target.value)
+            } />
+
+            <input type="submit" />
+
+        </form>
     )
 }
 
